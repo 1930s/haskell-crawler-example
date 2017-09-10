a00728720b0a397cec6f77ef37ee600764c13025
Vladimir Vinogradov <begray@gmail.com>
2017-09-10 20:51:49 +0300

    get wikipedia page using wreq

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Text.HandsomeSoup
+import Network.Wreq
+import Control.Lens
+import qualified Data.ByteString.Lazy as BS
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  r <- get "http://wikipedia.org"
+  BS.putStrLn $ (r ^. responseBody)
